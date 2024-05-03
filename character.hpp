71e0bb726983cb855f20cac34ebeafc05b042b52
Alejandro <barragan13al@gmail.com>
2024-05-02 23:20:55 -0700

    tried to fix errors

--- character.hpp
+++ character.hpp
@@ -10,7 +10,6 @@ int get_defense_bonus();
 
 
 class Character {
-
 private:
     int atk;
     int def;
@@ -23,25 +22,28 @@ public:
     virtual void attack(Character* target);
     virtual void special() = 0;
 
+    void set_attack(int atk){
+        atk = atk;
+    } 
+
     int get_attack() {
         return atk;
     }
-    void set_attack(int atk){
-	atk = atk;
+
+    void set_defense(int def){
+        def = def;
+    } 
+
+   int get_defense(){
+	return def;
     }
 
-    int get_defense() {
-        return def;
-    }
-    void set_defense(int def){
-	def = def;
+    void set_hp(int hp){
+	hp = hp;
     }
 
     int get_hp() {
         return hp;
-    }
-    void set_hp(int hp){
-	hp = hp;
     }
 
 };
@@ -56,18 +58,18 @@ private:
 
 public:
 
-    virtual void equip(Equip* equipment) override{
+    void equip(Equip* equipment) override{
 	set_attack(currentEquipment->get_attack_bonus());
         set_defense(currentEquipment->get_defense_bonus());
     }
 
-    virtual void attack(character* target) override{
+    void attack(character* target){
 	bool enemy;  // logic to determine if target is enemy
 
 	if(enemy){
-	   int updateHealth = target->get_hp() - get_attack();
+	   int updateHealth = get_hp() - get_attack();
 	   // apply damage to target
-	   target->set_hp(updateHealth);
+	   set_hp(updateHealth);
 	}
 
     }
